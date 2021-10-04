ced39a0588d0ffdca4097df0a94237c0e78e61d0
Evgenii.Korchagov <joedraiser@gmail.com>
2021-10-04 10:00:02 +0300

    Initial 15_6_2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
-    int result =9;
+    int result=9, x=-1, y=-1;
 
-    int array[] = {2, 7, 11, 15};
+    std::vector<int> array = {3, 7, -6, 15};
+
+    for(int i=0;i<array.size()-1;i++)
+    {
+        for(int j=i+1;j<array.size();j++)
+        {
+            if((array[i]+array[j])==result)
+            {
+                x=i;
+                y=j;
+                break;
+            }
+        }
+        if(x!=-1&&y!=-1)
+            break;
+    }
+
+    std::cout << array[x] << ' ' << array[y];
 }
