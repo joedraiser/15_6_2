2d28992d24e00eb355bcbaaf5e639bb1160665ee
Evgenii.Korchagov <joedraiser@gmail.com>
2021-10-04 09:40:51 +0300

    Initial 15_6_2

---
+++ cmake-build-debug/CMakeFiles/15_6_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "15_6_2.exe"
+  "15_6_2.exe.manifest"
+  "15_6_2.pdb"
+  "CMakeFiles/15_6_2.dir/main.cpp.obj"
+  "lib15_6_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/15_6_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    int result =9;
+
+    int array[] = {2, 7, 11, 15};
+}
